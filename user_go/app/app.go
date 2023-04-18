aea625061fcf406706f169b90e042b201885aa24
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2023-04-18 21:24:04 +0100

    started working on stateful app

--- user_go/app/app.go
+++ user_go/app/app.go
@@ -6,25 +6,32 @@ import (
 )
 
 type App struct {
-	State State
-	last  string
+	state State
 }
 
 func NewApp() App {
-	return App{State: Login}
+	return App{state: Login}
 }
 
 func (a *App) Run() {
 	for {
-		c := console.GetStr("Gimme")
+		switch a.state {
+		case Login:
+			{
+				a.state = a.login()
+			}
 
-		if c == "q" {
-			break
+		case Dashboard:
+			{
+				a.state = a.dashboard()
+			}
+
+		case Quit:
+			{
+				break
+			}
 		}
 
-		a.last = c
-		fmt.Println("You chose: ", a.last)
-		fmt.Println()
 	}
 
 	a.Cleanup()
@@ -32,8 +39,29 @@ func (a *App) Run() {
 
 func (a *App) Cleanup() {
 	fmt.Println("\n\nCTRL+C Intercepted\n\nrunning cleanup")
-	fmt.Println("last choice was: ", a.last)
 	fmt.Print("bye\n\n\n")
+}
+
+func (a *App) login() State {
+	println("\n\nLOGIN\n\n")
+	username := console.GetStr("username")
+	password := console.GetStr("password")
+	println(username, password)
+
+	return Dashboard
+}
+
+func (a *App) dashboard() State {
+	menu := make(map[string]string, 4)
+	menu["1"] = "Balance"
+	menu["2"] = "Withdraw"
+	menu["3"] = "Deposit"
+	menu["4"] = "Logout"
+	menu["5"] = "Logout"
+	c := console.ChooseFromMap("Menu", menu)
+	println(c)
+
+	return Login
 }
 
 type State = uint8
@@ -41,4 +69,5 @@ type State = uint8
 const (
 	Login State = iota
 	Dashboard
+	Quit
 )
